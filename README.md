14fd6b34e93031649f13a5d3995cd813235b86ef
jopemachine <jopemachine@naver.com>
2021-07-23 14:43:18 +0900

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# arvis-naver-search-plugin
+[![NPM download total](https://img.shields.io/npm/dt/arvis-naver-search-plugin)](http://badge.fury.io/js/arvis-naver-search-plugin)
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+[![PR's Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)](http://makeapullrequest.com)
+
+Simple cross-platform Naver search plugin for Arvis
+
+![](./demo.png)
+
+## Install
+
+Install by npm
+
+```
+$ npm i -g arvis-naver-search-plugin
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+const arvish = require('arvish');
+const encodeurl = require('encodeurl');
+
+module.exports = async function ({ inputStr }) {
+  const data = await arvish.fetch('http://ac.search.naver.com/nx/ac', {
+    query: {
+      st: 100,
+      r_format: 'json',
+      r_enc: 'UTF-8',
+      r_unicode: 0,
+      t_koreng: 1,
+      ans: 1,
+      run: 2,
+      rev: 4,
+      q: inputStr
+    },
+    maxAge: 86400000,
+    transform: (resp) => {
+      return resp.items[0].map((item) => {
+        const word = item[0];
+        const encodedUrl = encodeurl(word.split(' ').join('%20'));
+        return {
+          title: word,
+          arg: encodedUrl,
+          autocomplete: item[0],
+          quicklookurl: `http://search.naver.com/search.naver?ie=utf8&sm=stp_hty&where=se&query=${encodedUrl}`
+        };
+      });
+    }
+  });
+
+  return arvish.output(data);
+};
